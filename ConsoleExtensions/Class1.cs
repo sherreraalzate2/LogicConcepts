0cdf4a078f48978eb9465c7b725d2b1d578a1691
sherr <sherr@SANTY>
2025-08-21 20:40:32 -0500

    Add project files.

---
+++ ConsoleExtensions/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleExtensions
+{
+    public class ConsoleExtensions
+
+    {
+        public static int GetInt (string message)
+        {
+            Console.WriteLine (message);
+            var input = Console.ReadLine ();
+            if ( int.TryParse(input, out int result))
+            {
+                return result;
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ NumberslsOddOrNot/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ingrese número: ");
+var number = int.Parse(Console.ReadLine()!);
+if (number % 2 == 0)
+{
+    Console.WriteLine($"El numero: {number} es par.");
+}
+else
+{
+    Console.WriteLine($"El numero: {number} es impar.");
+}
+
+
